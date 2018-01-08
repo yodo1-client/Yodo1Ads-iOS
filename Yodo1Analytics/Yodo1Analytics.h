02531b0a470d66505cbfdf6940d4349ab34a73d7
yixian huang <huangyixian@yodo1.com>
2018-01-08 16:02:05 +0800

    Yodo1Analytics2.0.1

---
+++ Yodo1Analytics/Yodo1Analytics.h
@@ -0,0 +1,32 @@
+//
+//  Yodo1Analytics.h
+//
+//  Created by yixian huang on 2017/7/26.
+//
+//  sdk version 1.0.3
+//
+
+#ifndef Yodo1Analytics_h
+#define Yodo1Analytics_h
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Yodo1Analytics : NSObject
+
++ (id)instance;
+
+- (void)initWithAppKey:(NSString*)appKey
+              channelId:(NSString*)channelId;
+
+- (void)eventId:(NSString*)eventId
+      eventData:(NSDictionary*)eventData;
+
+- (void)setDebugMode:(BOOL)enableDebugMode;
+
+- (BOOL)getDebug;
+
+@end
+
+NS_ASSUME_NONNULL_END
+#endif /* Yodo1Analytics_h */
