6d5ba89c5b3902fbd7fcec302b7f42e9778eae48
yixian huang <huangyixian@yodo1.com>
2018-01-08 14:01:48 +0800

    Yodo1Interstitial 2.0.1

---
+++ Yodo1Interstitial/Yodo1InterstitialAdManager.h
@@ -0,0 +1,72 @@
+//
+//  Yodo1InterstitialAdManager.h
+//  localization_sdk_sample
+//
+//  Created by shon wang on 13-8-13.
+//  Copyright (c) 2013年 游道易. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol InterstitialAdDelegate <NSObject>
+
+@optional
+
+/**
+ Called after an interstitial has been loaded
+ */
+- (void)interstitialDidLoad;
+
+/**
+ Called after an interstitial has attempted to load but failed.
+ 
+ @param error The reason for the error
+ */
+- (void)interstitialDidFailToLoadWithError:(NSError *)error;
+
+/**
+ Called after an interstitial has been opened.
+ */
+- (void)interstitialDidOpen;
+
+/**
+ Called after an interstitial has been dismissed.
+ */
+- (void)interstitialDidClose;
+
+/**
+ Called after an interstitial has been displayed on the screen.
+ */
+- (void)interstitialDidShow;
+
+/**
+ Called after an interstitial has attempted to show but failed.
+ 
+ @param error The reason for the error
+ */
+- (void)interstitialDidFailToShowWithError:(NSError *)error;
+
+/**
+ Called after an interstitial has been clicked.
+ */
+- (void)didClickInterstitial;
+
+
+@end
+
+
+@interface Yodo1InterstitialAdManager: NSObject
+
+///Yodo1InterstitialAdManager单例
++ (Yodo1InterstitialAdManager*)sharedInstance;
+
+///初始化插屏sdk
+- (void)initInterstitalSDK:(id<InterstitialAdDelegate>)delegate;
+
+///显示插屏广告
+- (void)showAd;
+
+///插屏广告是否已经准备好
+- (BOOL)interstitialAdReady;
+
+@end
