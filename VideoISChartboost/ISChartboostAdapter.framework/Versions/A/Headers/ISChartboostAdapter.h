091eb713d8ce0b549ea909575c21f151119f7011
yixian huang <huangyixian@yodo1.com>
2018-01-10 10:24:43 +0800

    VideoISChartboost2.0.1

---
+++ VideoISChartboost/ISChartboostAdapter.framework/Versions/A/Headers/ISChartboostAdapter.h
@@ -0,0 +1,17 @@
+//
+//  Copyright (c) 2015 IronSource. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "IronSource/ISBaseAdapter+Internal.h"
+
+//System Frameworks For Chartboost Adapter
+
+@import Foundation;
+@import CoreGraphics;
+@import StoreKit;
+
+
+@interface ISChartboostAdapter : ISBaseAdapter
+
+@end
