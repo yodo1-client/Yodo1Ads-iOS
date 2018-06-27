fae408a3d058ecb03050f0795d4bb9ad4007d5e6
yixian huang <huangyixian@yodo1.com>
2018-06-27 11:30:09 +0800

    v3.0.2

--- Yodo1Ads.h
+++ Yodo1Ads.h
@@ -3,7 +3,7 @@
 //
 //
 //  Created by hyx on 17/7/14.
-//  v3.0.1
+//  v3.0.2
 //
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
--- Yodo1Ads.mm
+++ Yodo1Ads.mm
@@ -50,7 +50,7 @@ static VideoCallback s_videoCallback;
 const char* UNITY3D_YODO1ADS_METHOD     = "Yodo1U3dSDKCallBackResult";
 static NSString* kYodo1AdsGameObject    = @"Yodo1Ads";//默认
 
-NSString* const kYodo1AdsVersion       = @"3.0.1";
+NSString* const kYodo1AdsVersion       = @"3.0.2";
 
 typedef enum {
     Yodo1AdsTypeBanner          = 1001,//Banner
