79df3330af6a9f15853562eb0c80f57ea4a023f5
yixian huang <huangyixian@yodo1.com>
2018-08-02 15:36:00 +0800

    添加Banner 缩放大小的接口

--- Yodo1BannerManager.h
+++ Yodo1BannerManager.h
@@ -31,6 +31,9 @@ typedef enum {
 ///设置Banner偏移
 - (void)setBannerOffset:(CGPoint)point;
 
+///设置Banner Scale缩放倍数 x轴方向倍数sx,  y轴方向倍数sy
+- (void)setBannerScale:(CGFloat)sx sy:(CGFloat)sy;
+
 ///显示Banner广告
 - (void)showBanner;
 
