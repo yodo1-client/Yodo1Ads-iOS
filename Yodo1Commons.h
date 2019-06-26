72681fd22417ac1efcb76382593e332d88383b85
huangyixian <huangyixian@yodo1.com>
2019-06-26 17:12:29 +0800

    v3.0.4

--- Yodo1Commons.h
+++ Yodo1Commons.h
@@ -337,3 +337,18 @@ typedef void (^Yodo1CommonsUIBlockerTouchBlock)(Yodo1CommonsUIBlockerTouchEvent 
 
 
 @end
+
+@interface YD1Preferences : NSObject
++ (BOOL)hasKey:(NSString *)key;
++ (NSString *)getString:(NSString *)key;
++ (NSNumber *)getInteger:(NSString *)key;
++ (NSNumber *)getLong:(NSString *)key;
++ (NSNumber *)getBoolean:(NSString*)key;
++ (NSNumber *)getFloat:(NSString*)key;
++ (void)setString:(NSString*)value forKey:(NSString*)key;
++ (void)setInteger:(int)value forKey:(NSString*)key;
++ (void)setFloat:(float)value forKey:(NSString*)key;
++ (void)setBoolean:(BOOL)value forKey:(NSString*)key;
++ (void)setLong:(long)value forKey:(NSString*)key;
++ (void)removeKey:(NSString *)key;
+@end
--- Yodo1Commons.podspec
+++ Yodo1Commons.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
     s.name             = 'Yodo1Commons'
-    s.version          = '3.0.3'
+    s.version          = '3.0.4'
     s.summary          = '去掉多语言依赖[移到Yodo1UCenter里面]'
 
     s.description      = <<-DESC
