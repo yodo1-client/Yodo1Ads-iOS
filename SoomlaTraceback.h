f2af389fc6bb4b6895e2dfa44a287f404f983d45
yixian huang <huangyixian@yodo1.com>
2019-09-20 10:40:39 +0800

    v3.1.1

--- SoomlaTraceback.h
+++ SoomlaTraceback.h
@@ -11,7 +11,7 @@
 #import "TracebackAdvertising.h"
 #import "SoomlaConfig.h"
 
-#define SOOMLA_TRACEBACK_VERSION    @"5.0.2"
+#define SOOMLA_TRACEBACK_VERSION    @"5.0.4"
 
 @protocol SoomlaTracebackDelegate <NSObject>
 
