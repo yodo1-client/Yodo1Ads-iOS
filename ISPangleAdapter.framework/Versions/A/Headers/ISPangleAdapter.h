11aafc54784a46d6dc472a0d1e980be69a305e23
joyhubs <joyhubs@163.com>
2020-11-19 11:20:02 +0800

    v4.0.0

---
+++ ISPangleAdapter.framework/Versions/A/Headers/ISPangleAdapter.h
@@ -0,0 +1,34 @@
+//
+//  ISPangleAdapter.h
+//  ISPangleAdapter
+//
+//  Created by Guy Lis on 20/05/2019.
+//  Copyright © 2019 IronSource. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "IronSource/ISBaseAdapter+Internal.h"
+
+static NSString * const PangleAdapterVersion = @"4.1.9";
+static NSString * GitHash = @"eea10fb56";
+
+//System Frameworks For Pangle Adapter
+
+@import Accelerate;
+@import AdSupport;
+@import AVFoundation;
+@import CoreLocation;
+@import CoreMedia;
+@import CoreMotion;
+@import CoreTelephony;
+@import MapKit;
+@import MediaPlayer;
+@import MobileCoreServices;
+@import StoreKit;
+@import SystemConfiguration;
+@import UIKit;
+@import WebKit;
+
+@interface ISPangleAdapter : ISBaseAdapter
+
+@end
